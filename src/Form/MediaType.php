8270fddfd8df99b06ae45b73118c295c051c8bdf
OO <joyboy75>
2022-01-11 14:16:43 +0100

    Media

---
+++ src/Form/MediaType.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Media;
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class MediaType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('src', FileType::class, [
+                'mapped' => false
+            ])
+            ->add('alt')
+            ->add('title')
+            ->add('product', EntityType::class,[
+                'class' => Product::class,
+                'choice_label' => 'name'
+            ])
+            ->add('enregistrer', SubmitType::class);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Media::class,
+        ]);
+    }
+}
